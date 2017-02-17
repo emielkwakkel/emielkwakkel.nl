ed04a3df020e86318d48c3af2afdafa649169c8c
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2017-02-17 22:56:40 +0100

    Add header, main, menu components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
+// Import modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+// Import components
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MainComponent } from './blocks/main/main.component';
+import { HeaderComponent } from './blocks/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
