833792b56e2fd0246126f05ffd0e7d4c378a91ce
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2017-02-18 13:30:09 +0100

    Add Typewriters module

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,11 +1,15 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from "@angular/router";
+import { TypewriterModule } from "ng2-typewriter";
+
+// Components
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { MissionComponent } from './mission/mission.component';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { RouterModule } from "@angular/router";
 
 @NgModule({
   imports: [
@@ -14,8 +18,10 @@ import { RouterModule } from "@angular/router";
       { path: 'home', component: HomeComponent },
       { path: 'about', component: AboutComponent },
       { path: 'contact', component: ContactComponent },
-      { path: 'mission', component: MissionComponent }
+      { path: 'mission', component: MissionComponent },
+      { path: 'not-found', component: NotFoundComponent }
     ]),
+    TypewriterModule
   ],
   declarations: [AboutComponent, ContactComponent, MissionComponent, HomeComponent, NotFoundComponent]
 })
