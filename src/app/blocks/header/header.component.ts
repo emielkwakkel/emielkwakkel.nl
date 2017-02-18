75e06c39cce2168533180d01b3f99d2993336592
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2017-02-18 13:27:33 +0100

    Make sidebar toggable with animation

--- src/app/blocks/header/header.component.ts
+++ src/app/blocks/header/header.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'ek-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  public showSide : boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  toggleSide() : void {
+    this.showSide = !this.showSide;
   }
 
 }
