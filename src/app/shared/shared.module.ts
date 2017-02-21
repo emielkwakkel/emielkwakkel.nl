b1f7d5a8a576ca5f3d971cb7d7c1f967e99a0e61
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2017-02-21 22:12:21 +0100

    Add percentage component to shared

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PercentageComponent } from './chart/percentage/percentage.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [PercentageComponent]
 })
 export class SharedModule { }
