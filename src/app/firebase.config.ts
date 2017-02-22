0e887055a186d2a49f6573d89aac64aeb4de1649
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2017-02-22 17:13:28 +0100

    Typo

--- src/app/firebase.config.ts
+++ src/app/firebase.config.ts
@@ -1,7 +1,7 @@
 /**
  * Details are not committed for security purposes.
  * Add your own details in case you want to get data using Firebase.
- * @type {Object} Config for firebase
+ * @type {Object} Config for Firebase.
  */
 export const firebaseConfig = {
   apiKey: '',
