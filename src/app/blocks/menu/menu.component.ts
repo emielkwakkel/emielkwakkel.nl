8b9207be241327dfb841c5b12f77be2b5e258cbe
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2017-02-22 17:15:37 +0100

    Get menu items from Firebase.

--- src/app/blocks/menu/menu.component.ts
+++ src/app/blocks/menu/menu.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { Router, NavigationStart } from "@angular/router";
 import { BlocksService } from "../blocks.service";
+import { MenuItem } from "./menu.interface";
+import { FirebaseListObservable, AngularFire } from "angularfire2";
+import { Subscription } from "rxjs";
 
 @Component({
   selector: 'ek-menu',
   templateUrl: 'menu.component.html',
   styleUrls: ['menu.component.scss']
 })
-export class MenuComponent {
+export class MenuComponent implements OnDestroy {
+  menuItems: FirebaseListObservable<MenuItem[]>;
+  subscribe: Subscription;
 
-  constructor(private _router: Router, private _blocksService : BlocksService) {
+  constructor(
+    private _router: Router,
+    private _blocksService : BlocksService,
+    private _firebase : AngularFire) {
     // Detect navigation event
     _router.events.subscribe((navigate) => {
       if (navigate instanceof NavigationStart) {
@@ -17,5 +25,13 @@ export class MenuComponent {
         _blocksService.setShowSide(window.innerWidth >= 768);
       }
     });
+
+    this.subscribe = _firebase.database
+      .list('/menu-items')
+      .subscribe(items => this.menuItems = items);
+  }
+
+  ngOnDestroy() {
+    this.subscribe.unsubscribe();
   }
 }
---
+++ src/app/blocks/menu/menu.interface.ts
@@ -0,0 +1,6 @@
+export interface MenuItem {
+  $ref: any;
+  routerLink: any[],
+  iconClass: string,
+  text: string
+}
