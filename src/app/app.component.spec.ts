cf7f8082449d7df2f9523516f7071e39ef9ad77c
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2017-02-23 17:40:34 +0100

    Fix app unit test, check if header and main component are added.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 
 describe('AppComponent', () => {
   beforeEach(() => {
@@ -9,26 +10,41 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
     });
     TestBed.compileComponents();
   });
-
-  it('should create the app', async(() => {
+  it('should create the app.', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
+  // it(`should have as title 'app works!'`, async(() => {
+  //   const fixture = TestBed.createComponent(AppComponent);
+  //   const app = fixture.debugElement.componentInstance;
+  //   expect(app.title).toEqual('app works!');
+  // }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should add the header component to the dom.', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled
+      .querySelector('ek-header') !== null)
+      .toBe(true);
+    expect(compiled
+      .querySelector('ek-main') !== null)
+      .toBe(true);
+  }));
+
+  it('should add the main component to the dom.', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+
+    expect(compiled
+      .querySelector('ek-main') !== null)
+      .toBe(true);
   }));
 });
