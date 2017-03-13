a845247ba046cb8e9de7b14ef4b78bdcb8ded7d4
Emiel Kwakkel <emiel.kwakkel@sogeti.com>
2017-03-13 18:30:25 +0100

    Clean up component

--- src/app/shared/chart/percentage/percentage.component.ts
+++ src/app/shared/chart/percentage/percentage.component.ts
@@ -1,20 +1,12 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ek-percentage',
   templateUrl: './percentage.component.html',
   styleUrls: ['./percentage.component.scss']
 })
-export class PercentageComponent implements OnInit {
+export class PercentageComponent {
   @Input() percentage : number;
   @Input() title : string;
   @Input() description : string;
-
-  constructor() { }
-
-  ngOnInit() {
-    console.log(this.percentage);
-
-  }
-
 }
