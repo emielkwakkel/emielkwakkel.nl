c436f89861d454d45bd7826c3809c7c0a5148bd1
Emiel <mail@emielkwakkel.nl>
2019-06-16 18:36:31 +0200

    Create empty firebase config

---
+++ src/app/firebase.config.ts
@@ -0,0 +1,10 @@
+/**
+ * @type {Object} Config for Firebase.
+ */
+export const firebaseConfig = {
+  apiKey: 'INSERT_API_KEY',
+  authDomain: 'emiel-kwakkel.firebaseio.com',
+  databaseURL: 'https://emiel-kwakkel.firebaseio.com',
+  storageBucket: 'emiel-kwakkel.appspot.com',
+  messagingSenderId: '911552968147'
+};
