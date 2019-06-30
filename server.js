54067bec23d6b358edcefb7b96f89889770f10ec
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2019-06-30 22:40:59 +0200

    feat: implement gzip compression

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const compression = require('compression');
 const app = express();
 
 app.use(express.static(__dirname + '/dist/emielkwakkel'));
-
+app.use(compression());
 app.get('/*', function (req, res) {
   res.sendFile(__dirname + '/dist/emielkwakkel/index.html');
 });
