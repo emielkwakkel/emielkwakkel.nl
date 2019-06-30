4a941a26b1e1b971b807b2df49150834484e1d1e
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2019-06-30 23:29:43 +0200

    fix: fix allignment of new font-awesome icons

--- src/app/shared/highlight/highlight.component.ts
+++ src/app/shared/highlight/highlight.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'ek-highlight',
   templateUrl: './highlight.component.html',
   styleUrls: ['./highlight.component.scss']
 })
-export class HighlightComponent {
+export class HighlightComponent implements OnInit {
   @Input() icon : string;
+  ngOnInit() {
+    console.log('icon', this.icon)
+  }
 }
