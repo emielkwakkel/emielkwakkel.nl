c471a59aabc65acc5a5fe972b8c5dbc3ea980280
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2019-07-01 23:58:07 +0200

    feat: forceSSL

--- server.js
+++ server.js
@@ -2,9 +2,23 @@ const express = require('express');
 const compression = require('compression');
 const app = express();
 
+const forceSsl = (req, res, next) => {
+  if (req.headers['x-forwarded-proto'] !== 'https') {
+    return res.redirect(['https://', req.get('Host'), req.url].join(''));
+  }
+  return next();
+};
+
+app.configure(() => {
+  if (process.env.NODE_ENV === 'production') {
+    console.log('Production environment');
+    app.use(forceSsl);
+  }
+});
+
 app.use(express.static(__dirname + '/dist/emielkwakkel'));
 app.use(compression());
-app.get('/*', function (req, res) {
+app.get('/*', (req, res) =>{
   res.sendFile(__dirname + '/dist/emielkwakkel/index.html');
 });
 
