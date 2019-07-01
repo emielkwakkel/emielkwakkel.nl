2d47331eb209a6462aea1d33c28e1ec82057bb94
Emiel Kwakkel <emiel.kwakkel@rabobank.nl>
2019-07-02 00:05:50 +0200

    fix: express removed configure.. duh!

--- server.js
+++ server.js
@@ -3,21 +3,18 @@ const compression = require('compression');
 const app = express();
 
 const forceSsl = (req, res, next) => {
-  if (req.headers['x-forwarded-proto'] !== 'https') {
+  if (process.env.NODE_ENV !== 'production') return;
+  console.log('Production environment');
+
+  if (req.headers['x-forwarded-proto'] !== 'https')
     return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
+
   return next();
 };
 
-app.configure(() => {
-  if (process.env.NODE_ENV === 'production') {
-    console.log('Production environment');
-    app.use(forceSsl);
-  }
-});
-
 app.use(express.static(__dirname + '/dist/emielkwakkel'));
 app.use(compression());
+app.use(forceSsl);
 app.get('/*', (req, res) =>{
   res.sendFile(__dirname + '/dist/emielkwakkel/index.html');
 });
